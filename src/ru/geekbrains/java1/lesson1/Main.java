6854f34b342a6a81dac3c34c38672118154d961b
unknown <zlovred12@gmail.com>
2022-06-11 19:19:25 +0300

    'initialize'

---
+++ src/ru/geekbrains/java1/lesson1/Main.java
@@ -0,0 +1,21 @@
+package ru.geekbrains.java1.lesson1;
+
+public class Main {
+
+    private static void initialize(){
+        byte b = 1;
+        short s = 2;
+        int i = 4;
+        long l = 8;
+
+        float f = 1.0f;
+        double d = 2.0;
+
+        char c = 'c';
+        boolean bool = true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
