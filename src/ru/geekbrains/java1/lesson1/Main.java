88c3cb5f8d702c6e87dc2a452aca981b1af0b650
unknown <zlovred12@gmail.com>
2022-06-11 19:46:27 +0300

    'isLeap'

--- src/ru/geekbrains/java1/lesson1/Main.java
+++ src/ru/geekbrains/java1/lesson1/Main.java
@@ -2,6 +2,38 @@ package ru.geekbrains.java1.lesson1;
 
 public class Main {
 
+    private static void isLeap(int year){
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+            System.out.println("leap");
+        } else {
+            System.out.println("no leap");
+        }
+    }
+
+    private static String hello(String name){
+        return "Hello:" + " " + name;
+    }
+
+    private static boolean toNegative(int number){
+        boolean result = false;
+        if (number < 0){
+            result = true;
+        }
+        return result;
+    }
+
+    private static void isPositive(int number){
+        if (number >= 0){
+            System.out.println("positive");
+        }else {
+            System.out.println("negative");
+        }
+    }
+
+    private static boolean checkSum(int first, int second){
+        return first + second >= 10 && first + second <= 20;
+    }
+
     private static int calculate(int a, int b, int c, int d){
         return a * (b + (c / d));
     }
@@ -20,6 +52,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-
+        isLeap(1612);
     }
 }
