795e58539d5f41506b00144b53e5f402f8bf9072
unknown <zlovred12@gmail.com>
2022-06-12 16:00:11 +0300

    guessWord

--- src/ru/geekbrains/java1/lesson3/Main.java
+++ src/ru/geekbrains/java1/lesson3/Main.java
@@ -1,10 +1,48 @@
 package ru.geekbrains.java1.lesson3;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
+
+    private static void guessWord(){
+        String[] words = {"apple", "orange", "lemon", "banana",
+                "apricot", "avocado", "broccoli", "carrot", "cherry",
+                "garlic", "grape", "melon", "leak", "kiwi", "mango",
+                "mushroom", "nut", "olive", "pea", "peanut", "pear",
+                "pepper", "pineapple", "pumpkin", "potato"};
+        Random rnd = new Random();
+        int number = rnd.nextInt(words.length);
+        Scanner sc = new Scanner(System.in);
+        String sample = "###############";
+
+        String[] arr = sample.split("");
+
+        while (true){
+            String word = words[number];
+            System.out.println("enter you word");
+            String guess = sc.nextLine();
+
+
+            for (int i = 0; i < guess.length() ; i++) {
+                if (guess.charAt(i) == word.charAt(i)){
+                    arr[i] = String.valueOf(guess.charAt(i));
+                }
+            }
+            String str = String.join(",", arr);
+            str = str.replaceAll(",", "");
+            System.out.println(str);
+            if (guess.equals(word)){
+                System.out.println("win");
+                break;
+            }
+
+        }
+
+
+    }
     private static void guessNumber(){
         int amount = 0;
         int tries = 3;
@@ -28,6 +66,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        guessNumber();
+        guessWord();
     }
 }
